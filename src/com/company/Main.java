6bd5c897d81e625b491f5d75a2a98ac5e301ecbf
jakub_modrzejewski <jakub_modrzejewski@wp.pl>
2015-12-05 17:13:29 +0100

    kkk

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         double a, b;
-    System.out.println("Witaj w kalkulatorze. Mozesz tutaj wykonac operacje dodawania, odejmowania, mnozenia i dzielenia");
+        System.out.println("Witaj w kalkulatorze. Mozesz tutaj wykonac operacje dodawania, odejmowania, mnozenia i dzielenia");
         System.out.println("Podaj pierwsza liczbe.");
         Scanner liczba = new Scanner(System.in);
         a = liczba.nextDouble();
@@ -16,23 +16,23 @@ public class Main {
         System.out.println("Podaj druga liczbe");
         Scanner liczba2 = new Scanner(System.in);
         b = liczba2.nextInt();
-    switch (znak){
-        case "+":
-            System.out.println(a + b);
-            break;
-        case "-":
-            System.out.println(a - b);
-            break;
-        case "*":
-            System.out.println(a * b);
-            break;
-        case "/":
-            if (b==0){
-            System.out.println("Nie dziel przez zero!");
-            } else {
-            System.out.println(a / b);
-            break;
+        switch (znak) {
+            case "+":
+                System.out.println(a + b);
+                break;
+            case "-":
+                System.out.println(a - b);
+                break;
+            case "*":
+                System.out.println(a * b);
+                break;
+            case "/":
+                if (b == 0) {
+                    System.out.println("Nie dziel przez zero!");
+                } else {
+                    System.out.println(a / b);
+                    break;
+                }
+        }
     }
-    }
-}
 }
